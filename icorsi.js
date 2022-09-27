38111df2c4bafe2ee35aeffd6afd815eeadd2fd7
PezzottiCarlo <carlo.pezzotti@student.supsi.ch>
2022-09-27 10:31:21 +0200

    feat: :beers: auto submit attendace

--- icorsi.js
+++ icorsi.js
@@ -1,58 +1,130 @@
-const fetch = require('node-fetch');
-const puppeteer = require('puppeteer');
+const fetch = require("node-fetch");
+const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 module.exports = class ICorsi {
+  cookies;
+  session;
 
-    session;
-    sessionInfo;
+  async init(sess) {
+    this.session = sess.session;
+    this.cookies = sess.cookies;
+  }
 
-    async login(username, password) {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://www.icorsi.ch/Shibboleth.sso/Login?entityID=https%3A%2F%2Flogin2.supsi.ch%2Fidp%2Fshibboleth&target=https%3A%2F%2Fwww.icorsi.ch%2Fauth%2Fshibboleth%2F');
-        await page.type('#username', username);
-        await page.type('#password', password);
-        let elHandleArray = await page.$$('button')
-        let elHandle = elHandleArray[0]
-        await elHandle.click();
-        await page.waitForSelector("#actionmenuaction-6",{timeout:5000})
-        let cookies = await page.cookies();
-        await browser.close();
-        this.session = cookies;
-        return await this.getSessInfo();
+  async login(username, password) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(
+      "https://www.icorsi.ch/Shibboleth.sso/Login?entityID=https%3A%2F%2Flogin2.supsi.ch%2Fidp%2Fshibboleth&target=https%3A%2F%2Fwww.icorsi.ch%2Fauth%2Fshibboleth%2F"
+    );
+    await page.type("#username", username);
+    await page.type("#password", password);
+    let elHandleArray = await page.$$("button");
+    let elHandle = elHandleArray[0];
+    await elHandle.click();
+    await page.waitForSelector("#actionmenuaction-6", { timeout: 5000 });
+    this.cookies = await page.cookies();
+    await browser.close();
+    await this.getSession();
+    await this.exportSession();
+  }
+
+  async getSession() {
+    let res = await fetch("https://www.icorsi.ch/my/", {
+      headers: {
+        Cookie: this.cookies.map((c) => `${c.name}=${c.value}`).join("; "),
+      },
+    });
+    let body = await res.text();
+    this.session = JSON.parse(body.split("M.cfg = ")[1].split(";")[0]);
+    return this.session.sesskey;
+  }
+
+  async getCourses() {
+    let res = await fetch(
+      `https://www.icorsi.ch/lib/ajax/service.php?sesskey=${this.session.sesskey}&info=core_course_get_enrolled_courses_by_timeline_classification`,
+      {
+        headers: {
+          Cookie: this.cookies.map((c) => `${c.name}=${c.value}`).join("; "),
+        },
+        body: '[{"index":0,"methodname":"core_course_get_enrolled_courses_by_timeline_classification","args":{"offset":0,"limit":0,"classification":"all","sort":"fullname","customfieldname":"","customfieldvalue":""}}]',
+        method: "POST",
+      }
+    );
+    let courses = await res.json();
+    if (courses[0].error) return [];
+    return courses[0].data.courses;
+  }
+
+  async getCourseAttedancesLink(id) {
+    let res = await fetch(`https://www.icorsi.ch/course/view.php?id=${id}`, {
+      headers: {
+        Cookie: this.cookies.map((c) => `${c.name}=${c.value}`).join("; "),
+      },
+    });
+    let body = await res.text();
+    let attedances = body.match(
+      /https:\/\/www.icorsi.ch\/mod\/attendance\/view.php\?id=[0-9]+/g
+    );
+    return attedances == null ? false : attedances;
+  }
+
+  async getEvents() {
+    let res = await fetch(
+      `https://www.icorsi.ch/lib/ajax/service.php?sesskey=${this.session.sesskey}&info=core_calendar_get_action_events_by_timesort`,
+      {
+        headers: {
+          Cookie: this.cookies.map((c) => `${c.name}=${c.value}`).join("; "),
+        },
+        body: '[{"index":0,"methodname":"core_calendar_get_action_events_by_timesort","args":{"limitnum":26,"timesortfrom":1648677600,"limittononsuspendedevents":true}}]',
+        method: "POST",
+      }
+    );
+    let events = await res.json();
+    if (courses[0].error) return [];
+    return events[0].data.events;
+  }
+
+  async exportSession(filename) {
+    if (!filename) {
+      filename = "session.json";
     }
+    await fs.writeFileSync(
+      filename,
+      JSON.stringify({ session: this.session, cookies: this.cookies })
+    );
+  }
 
-    async getSessInfo() {
-        let res = await fetch('https://www.icorsi.ch/my/', {
-            headers: {
-                'Cookie': this.session.map(c => `${c.name}=${c.value}`).join('; ')
-            }
-        })
-        let body = await res.text();
-        this.sessionInfo = JSON.parse(body.split("M.cfg = ")[1].split(";")[0]);
-        return (this.sessionInfo.sesskey) 
-    }
+  async getAttendanceSessid(link) {
+    let res = await fetch(link, {
+      headers: {
+        Cookie: this.cookies.map((c) => `${c.name}=${c.value}`).join("; "),
+      },
+    });
+    //read page content from res
+    let body = await res.text();
+    //extract from content "https://www.icorsi.ch/mod/attendance/attendance.php?sessid=29800"
+    let extractedString = body.match(/https:\/\/www.icorsi.ch\/mod\/attendance\/attendance.php\?sessid=[0-9]+/g);
+    if (extractedString == null) return null;
+    if (extractedString.length == 0) return null;
+    if (extractedString.length > 1) return null;
+    if (extractedString[0] == null) return null;
+    if (extractedString[0].length == 0) return null;
+    let sessid = extractedString[0].split("=")[1]
+    return sessid;
+  }
 
-    async getCourses() {
-        let res = await fetch(`https://www.icorsi.ch/lib/ajax/service.php?sesskey=${this.sessionInfo.sesskey}&info=core_course_get_enrolled_courses_by_timeline_classification`, {
-            "headers": {
-                'Cookie': this.session.map(c => `${c.name}=${c.value}`).join('; ')
-            },
-            "body": "[{\"index\":0,\"methodname\":\"core_course_get_enrolled_courses_by_timeline_classification\",\"args\":{\"offset\":0,\"limit\":0,\"classification\":\"all\",\"sort\":\"fullname\",\"customfieldname\":\"\",\"customfieldvalue\":\"\"}}]",
-            "method": "POST"
-        });
-        let courses = await res.json();
-        return courses[0].data.courses;
-    }
-
-    async getCourseAttedancesLink(id) {
-        let res = await fetch(`https://www.icorsi.ch/course/view.php?id=${id}`, {
-            "headers": {
-                'Cookie': this.session.map(c => `${c.name}=${c.value}`).join('; ')
-            }
-        })
-        let body = await res.text();
-        let attedances = body.match(/href="https:\/\/www.icorsi.ch\/mod\/attendance\/view.php\?id=[0-9]+"/g);
-        return (attedances==null)?[]:attedances;
-    }
-}
+  async submitAttendace(sessId) {
+    let res = await fetch("https://www.icorsi.ch/mod/attendance/attendance.php", {
+      "headers": {
+        "content-type": "application/x-www-form-urlencoded",
+        "Referer": `https://www.icorsi.ch/mod/attendance/attendance.php?sessid=${sessId}&sesskey=${this.session.sesskey}`,
+        "Referrer-Policy": "strict-origin-when-cross-origin",
+        Cookie: this.cookies.map((c) => `${c.name}=${c.value}`).join("; "),
+      },
+      body: `sessid=${sessId}&sesskey=${this.session.sesskey}&sesskey=${this.session.sesskey}&_qf__mod_attendance_form_studentattendance=1&mform_isexpanded_id_session=1&status=663&submitbutton=Save+changes`,
+      method: "POST"
+    });
+    return true;
+  }
+};
--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const ICorsi = require('./icorsi.js');
+const ICorsi = require("./icorsi.js");
+const fs = require("fs");
 
-async function main(){
-    let icorsi = new ICorsi();
-    let username = process.argv[2];
-    let password = process.argv[3];
-    console.log("Logging in...");
-    if(await icorsi.login(username, password)){
-        console.log("Logged in!");
-        let courses = await icorsi.getCourses();
-        console.log(courses.length)
-        for(let course of courses){
-            console.log(course.fullname);
-            let attedances = await icorsi.getCourseAttedancesLink(course.id);
-            for(let attedance of attedances){
-                console.log(attedance);
-            }
-        }
-    }else{
-        console.log("Login failed");
+async function main() {
+  let icorsi = new ICorsi();
+  let username = "carlo.pezzotti@student.supsi.ch";
+  let password = process.env.icorsiPass;
+  console.log("Logging in...");
+  //await icorsi.init(JSON.parse(fs.readFileSync("session.json")));
+  await icorsi.login(username, password);
+  console.log("Logged in!");
+  let courses = await icorsi.getCourses();
+  console.log("Courses: " + courses.length);
+  for (let i = 0; i < courses.length; i++) {
+    console.log("Name: \t" + courses[i].fullname);
+    console.log("Id: \t" + courses[i].id);
+    let attendanceLink = await icorsi.getCourseAttedancesLink(courses[i].id);
+    if (attendanceLink) {
+      console.log("Attendance: \t" + attendanceLink);
+      let sessId = await icorsi.getAttendanceSessid(attendanceLink);
+      if (sessId) {
+        console.log("Attendance id \t" + sessId);
+        console.log("https://www.icorsi.ch/mod/attendance/attendance.php?sessid=" + sessId);
+        await icorsi.submitAttendace(sessId);
+      }
     }
+    console.log("\n\n");
+  }
 }
-
 main();
